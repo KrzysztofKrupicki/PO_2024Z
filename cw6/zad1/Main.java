ec0b5e0c3c08480efc63433c450b6a5c7910687f
KrzysztofKrupicki <>
2024-11-07 19:53:02 +0100

    added cw6

---
+++ cw6/zad1/Main.java
@@ -0,0 +1,8 @@
+package zad1;
+
+public class Main {
+    public static void main(String[] args) {
+        BookDT0 b1 = new BookDT0("1984", "George Orwell", 39.99, 1984);
+        BookDT0 b12 = new BookDT0("Wiedźmin", "Andrzej Sapkowski", 19.99, 2019);
+    }
+}
---
+++ cw6/zad4/Main.java
@@ -0,0 +1,8 @@
+package zad4;
+
+public class Main {
+    public static void main(String[] args) {
+        Car c1 = new Car("Toyota", "Corozja", 4);
+        System.out.println(c1.fuelCost(5, 50));
+    }
+}
